09bcf0e1efb8279c3ce3cf7f2f7c8a9490f10ff6
Ryan Hardeman <Ryan Hardeman>
2017-07-14 18:43:32 -0400

    Added twitter keys and spotify song to random.txt

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'jN19PCYIJPkEJyh6gBVChd7PJ',
+  consumer_secret: 'ZLUMQQFfOfIzfgJgYhgrzIcIOhKJrFhJAhyBoufJvHob3p6c2N',
+  access_token_key: '885991010769268736-zsVbSCMOAF9441mdUrkxssQZYVNcOXV',
+  access_token_secret: 'GgTJ7YdfFgQlG2mcXhsM8lQUTnczLpJzjv0rjs7gVGoOL',
 }
