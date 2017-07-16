47f97647378901d20d4bd4373bd99d47640b42c0
Ryan Hardeman <Ryan Hardeman>
2017-07-15 21:16:29 -0400

    Twitter complete

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '885991010769268736-zsVbSCMOAF9441mdUrkxssQZYVNcOXV',
   access_token_secret: 'GgTJ7YdfFgQlG2mcXhsM8lQUTnczLpJzjv0rjs7gVGoOL',
 }
+
+exports.spotifyKeys = {
+  consumer_key: 'b3109805836748fd9008af459eeaacdf',
+  consumer_secret: '3112ff859cd347d29a08e11488b86876',
+}
--- liri.js
+++ liri.js
@@ -1,3 +1,45 @@
+var fs = require('fs');
+
+var twitter = require('twitter');
 var twitterKeys = require('./keys.js');
 
-var spotify = require('spotify');
+var client = new twitter({
+  consumer_key: twitterKeys.twitterKeys.consumer_key,
+  consumer_secret: twitterKeys.twitterKeys.consumer_secret,
+  access_token_key: twitterKeys.twitterKeys.access_token_key,
+  access_token_secret: twitterKeys.twitterKeys.access_token_secret
+});
+
+var searchCommand = process.argv[2];
+var searchTerm = process.argv[3];
+runLiri(searchCommand, searchTerm);
+
+function runLiri(searchCommand, searchTerm){
+	switch (searchCommand){
+		case 'my-tweets':
+			twitterPosts()
+			break;
+		case 'spotify-this-song':
+			sptofySearch(searchTerm);
+			break;
+		case 'movie-this':
+			movieInfo(searchTerm);
+			break;
+		case 'do-what-it-says':
+			fileReader();
+			break;
+		default:
+		console.log('Command Unknown!')
+	};
+};
+
+function twitterPosts() {
+	var params = {screen_name: 'rkhardeman', count: '20'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	  	tweets.forEach(function(element) {
+		    console.log(element.created_at + ": " + element.text);
+		});
+	  }
+	});
+};
