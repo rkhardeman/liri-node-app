189034729c15d87b73284155dce78a58dfab3c53
Ryan Hardeman <Ryan Hardeman>
2017-07-15 22:00:55 -0400

    OMDB working with one word movie title...

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var request = require("request");
 
 var twitter = require('twitter');
 var twitterKeys = require('./keys.js');
@@ -19,11 +20,11 @@ function runLiri(searchCommand, searchTerm){
 		case 'my-tweets':
 			twitterPosts()
 			break;
-		case 'spotify-this-song':
-			sptofySearch(searchTerm);
-			break;
 		case 'movie-this':
 			movieInfo(searchTerm);
+			break;
+		case 'spotify-this-song':
+			sptofySearch(searchTerm);
 			break;
 		case 'do-what-it-says':
 			fileReader();
@@ -42,4 +43,23 @@ function twitterPosts() {
 		});
 	  }
 	});
+};
+
+function movieInfo(searchTerm){
+
+		var movie = searchTerm;
+		var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+
+		request(queryURL, (error, response, body) =>{
+		
+		console.log("Title: " + JSON.parse(body).Title);
+	    console.log("Released: " + JSON.parse(body).Year);
+	    console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+	    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+	    console.log("Country Released: " + JSON.parse(body).Country);
+	    console.log("Original Language: " + JSON.parse(body).Language);
+	    console.log("Plot: " + JSON.parse(body).Plot);
+	    console.log("Actors: " + JSON.parse(body).Actors);
+		});
+
 };
